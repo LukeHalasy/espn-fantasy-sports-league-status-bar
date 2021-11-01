2e284d806ab1eae104d1f72c87fa644b5a78794e
LukeHalasy <lhalasy@gmail.com>
2021-11-01 19:05:19 -0400

    Automatically syncs the component dist with the extension

--- extension/content-script.js
+++ extension/content-script.js
@@ -1,5 +1,5 @@
 var s = document.createElement('script');
-s.src = chrome.runtime.getURL('js/espn-fantasy-sports-league-status-bar-component.esm.js');
+s.src = chrome.runtime.getURL('componentDist/espn-fantasy-sports-league-status-bar-component/espn-fantasy-sports-league-status-bar-component.esm.js');
 s.type = "module"
 s.onload = function() {
     this.remove();
