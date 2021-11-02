a1a4d7b6bb3d47840b0c6d8472000b0e4079ba7e
LukeHalasy <lhalasy@gmail.com>
2021-11-02 00:42:59 -0400

    Got the status bar to kind of be at the bottom of a video

--- extension/content-script.js
+++ extension/content-script.js
@@ -2,12 +2,58 @@ var s = document.createElement('script');
 s.src = chrome.runtime.getURL('componentDist/espn-fantasy-sports-league-status-bar-component/espn-fantasy-sports-league-status-bar-component.esm.js');
 s.type = "module"
 s.onload = function() {
-    this.remove();
+  this.remove();
 };
+
 (document.head || document.documentElement).appendChild(s);
+
+
+window.addEventListener ("load", myMain, false);
+
+function myMain(evt) {
+  fetch(chrome.runtime.getURL('/index.html')).then(r => r.text()).then(html => {
+    const videoTags = document.getElementsByTagName('video');
+    // console.log(videoTags[0].parentNode);
+    const parentDiv = videoTags[0].closest('div');
+    // console.log(videoTags[0].parentNode.getBoundingClientRect());
+
+    var x = parentDiv.getBoundingClientRect().left
+    var y = parentDiv.getBoundingClientRect().bottom
+    var width = parentDiv.getBoundingClientRect().width
+    var statusBarHeight = 20
+
+    var html = `<status-bar-component style="position: relative; left: 0px; top: ${parentDiv.getBoundingClientRect().height - statusBarHeight}px" width="${width}px" height="${statusBarHeight}px"></status-bar-component>`;
+    
+    
+    // var child = document.createElement
+    // document.body.insertAdjacentHTML("beforeend", html);
+    parentDiv.insertAdjacentHTML("beforeend", html);
+
+    // not using innerHTML as it would break js event listeners of the page
+  });
+}
+
+
+
+
+
+
+
+/*
+const videoTags = document.getElementsByTagName('video');
+// console.log(videoTags[0].getBoundingClientRect());
+
+const statusBar = document.createElement('status-bar-component');
+statusBar.style.position = 'absolute';
+// statusBar.style.objectPosition = `${videoTags[0].getBoundingClientRect().left}px ${videoTags[0].getBoundingClientRect().top}px`
+ statusBar.style.objectPosition = `400px 300px`
+document.body.appendChild(statusBar);
+*/
+
 
 /*
 var scripts = ['js/status-bar-component.esm.js', 'js/status-bar-component.js'];
+
 
 for (index = 0; index < scripts.length; ++index) {
   var script = document.createElement('script');
@@ -30,9 +76,17 @@ for (index = 0; index < scripts.length; ++index) {
 }
 */
 
-fetch(chrome.runtime.getURL('/index.html')).then(r => r.text()).then(html => {
-  console.log("HERE!!");
-  document.body.insertAdjacentHTML('beforeend', html);
-  // not using innerHTML as it would break js event listeners of the page
-});
+
+/*
+if (videoTags) {
+  for( var i = 0; i < videoTags.length; i++ ){
+    console.log(videoTags[i]);
+  }
+}
+*/
+
+
+
+
+
 
