629630132f96cc38eedf15f6aa3c7056aaba5912
LukeHalasy <lhalasy@gmail.com>
2021-11-03 12:42:18 -0400

    Got the bar to attach itself to the bottom of a video in fullscreen

--- extension/content-script.js
+++ extension/content-script.js
@@ -1,3 +1,4 @@
+// Load in the stencil component code into the head
 var s = document.createElement('script');
 s.src = chrome.runtime.getURL('componentDist/espn-fantasy-sports-league-status-bar-component/espn-fantasy-sports-league-status-bar-component.esm.js');
 s.type = "module"
@@ -7,34 +8,152 @@ s.onload = function() {
 
 (document.head || document.documentElement).appendChild(s);
 
+// utility function to wrap an element into a div
+var wrap = function (toWrap) {
+    var height = toWrap.getBoundingClientRect().height;
+    var wrapper = document.createElement('div');
+    /// wrapper.style.height = `${height}px`;
 
-window.addEventListener ("load", myMain, false);
+    // wrapper.style.position = "relative";
+    wrapper.style.overflow = 'auto';
+    toWrap.parentNode.appendChild(wrapper);
+    wrapper.appendChild(toWrap);
+    return wrapper;
+};
 
-function myMain(evt) {
-  fetch(chrome.runtime.getURL('/index.html')).then(r => r.text()).then(html => {
-    const videoTags = document.getElementsByTagName('video');
-    // console.log(videoTags[0].parentNode);
-    const parentDiv = videoTags[0].closest('div');
-    // console.log(videoTags[0].parentNode.getBoundingClientRect());
+function test() {
+  console.log("CALLED");
+  const video = document.getElementsByTagName('video')[0];
+  const parentDiv = video.closest('div');
+  parentDiv.style.paddingBottom = '50px';
+}
+
+function createStatusBar() {
+    const video = document.getElementsByTagName('video')[0];
+    ["fullscreenchange", "webkitfullscreenchange", "mozfullscreenchange", "msfullscreenchange"].forEach(
+        eventType => document.addEventListener(eventType, test, false)
+    );
+
+    // create parentDiv that wraps around the video
+    const parentDiv = wrap(video);
+
+    var x = parentDiv.getBoundingClientRect().left;
+    var width = parentDiv.getBoundingClientRect().width;
+    var statusBarHeight = 20;
+
+    var html = `<status-bar-component style="position: absolute; width: 100%; left: 0px; bottom: 0px; z-index: 289" height="${statusBarHeight}px"></status-bar-component>`;
+    
+    video.insertAdjacentHTML("afterend", html);
+    /*
+    video.addEventListener('click', function() {
+      console.log("YO");
+      console.log(parentDiv);
+      parentDiv.webkitRequestFullscreen();
+      videoTags[0].style.paddingBottom = "50px";
+
+      const statusBarComponent = document.getElementsByTagName('status-bar-component')[0];
+      statusBarComponent.style.zIndex = 21;
+      // openFullscreen(parentDiv);
+    }, false)
+    */
+    // not using innerHTML as it would break js event listeners of the page
+}
+
+function resizeStatusBar() {
+  console.log("Resize called");
+    const video = document.getElementsByTagName('video')[0];
+    const parentDiv = video.closest('div');
 
     var x = parentDiv.getBoundingClientRect().left
     var y = parentDiv.getBoundingClientRect().bottom
-    var width = parentDiv.getBoundingClientRect().width
+    console.log('video');
+    console.log(video);
+    var width = video.getBoundingClientRect().width
+    console.log('width', width);
     var statusBarHeight = 20
 
-    var html = `<status-bar-component style="position: relative; left: 0px; top: ${parentDiv.getBoundingClientRect().height - statusBarHeight}px" width="${width}px" height="${statusBarHeight}px"></status-bar-component>`;
-    
-    
-    // var child = document.createElement
-    // document.body.insertAdjacentHTML("beforeend", html);
-    parentDiv.insertAdjacentHTML("beforeend", html);
+    const statusBarComponent = document.getElementsByTagName('status-bar-component')[0];
 
-    // not using innerHTML as it would break js event listeners of the page
+    statusBarComponent.style.left = '0px';
+    statusBarComponent.style.top = `${video.getBoundingClientRect().height - statusBarHeight}px`;
+    // statusBarComponent.width = `${width}px`;
+}
+
+function videoLoaded() {
+  createStatusBar();     
+  window.addEventListener("resize", resizeStatusBar);
+  
+}
+
+
+// be sure to wait until a video has a loaded on the page to do anything
+if (document.getElementsByTagName('video')[0] != undefined) {
+  videoLoaded();
+} else {
+  var observer = new MutationObserver(function(mutations) {
+    if (document.getElementsByTagName('video')[0] != undefined) {
+      videoLoaded();
+      observer.disconnect()
+    }
   });
+
+  observer.observe(document, {attributes: false, childList: true, characterData: false, subtree:true});
 }
 
 
 
+
+
+
+
+function openFullscreen(div) {
+  console.log("hitting")
+  var elem = div
+  console.log(elem)
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.mozRequestFullScreen) { /* Firefox */
+    elem.mozRequestFullScreen();
+  } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari & Opera */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE/Edge */
+    elem.msRequestFullscreen();
+  }
+}
+
+function closeFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.webkitExitFullscreen) { /* Safari */
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) { /* IE11 */
+    document.msExitFullscreen();
+  }
+}
+
+function onFullScreen(e) {
+  var isFullscreenNow = document.webkitFullscreenElement !== null
+  if (isFullscreenNow) {
+    closeFullscreen();    
+    
+    const videoTags = document.getElementsByTagName('video');
+    // console.log(videoTags[0].parentNode);
+    const parentDiv = videoTags[0].closest('div');
+    openFullscreen(parentDiv);
+  }
+  // document.fullscreenElement will point to the element that
+  // is in fullscreen mode if there is one. If there isn't one,
+  // the value of the property is null.
+  /*
+  console.log('DISPLAY FULL SCREEN');
+
+  const statusBarComponent = document.getElementsByTagName('status-bar-component')[0];
+  console.log(statusBarComponent);
+  statusBarComponent.style.left = '0px';
+  statusBarComponent.style.top = `${window.screen.height - statusBarComponent.height}px`;
+  statusBarComponent.width = window.screen.width;
+  */
+};
 
 
 
